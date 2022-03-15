edc92e384e66d619333afcd9ce834a388876d399
gaoxiaoxinggxx <1129069725@qq.com>
2022-03-15 23:34:55 +0800

    feat: add login page

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,28 +4,60 @@
       <div class="title">Online Marking Test Day Operation Portal</div>
     </div>
     <div class="card">
-      <div class="title">Log in</div>
-      <a-form ref="formRef" layout="vertical">
-        <a-form-item label="User">
-          <a-input />
-        </a-form-item>
-        <a-form-item label="Password">
-          <a-input  type="password" />
-        </a-form-item>
-        <a-form-item>
-          <a-button  type="primary" block @click="">Log in</a-button>
-        </a-form-item>
-      </a-form>
+      <div class="title">S M S</div>
+
+      <a-tabs v-model:activeKey="activeKey" style="text-align: center;">
+        <a-tab-pane key="1" tab="用户密码登录">
+          <a-form ref="formRef">
+            <a-form-item>
+              <a-input placeholder="账 户" size="large">
+                <template #prefix>
+                  <user-outlined type="user" style="color: rgba(0, 0, 0, 0.45)" />
+                </template>
+              </a-input>
+            </a-form-item>
+            <a-form-item>
+              <a-input placeholder="密 码" type="password" size="large">
+                <template #prefix>
+                  <lock-outlined type="password" style="color: rgba(0, 0, 0, 0.45)" />
+                </template>
+              </a-input>
+            </a-form-item>
+          </a-form>
+        </a-tab-pane>
+        
+        <a-tab-pane key="2" tab="手机号登录" force-render>
+            <a-form ref="formRef">
+            <a-form-item>
+              <a-input placeholder="手机号" size="large">
+                <template #prefix>
+                  <tablet-outlined style="color: rgba(0, 0, 0, 0.45)" />
+                </template>
+              </a-input>
+            </a-form-item>
+            <a-form-item>
+              <a-input placeholder="验证码" size="large">
+                <template #prefix>
+                  <mail-outlined style="color: rgba(0, 0, 0, 0.45)" />
+                </template>
+              </a-input>
+            </a-form-item>
+          </a-form>
+        </a-tab-pane>
+      </a-tabs>
+      <a-button size="large" type="primary" block>登 录</a-button>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { useForm } from "ant-design-vue/lib/form";
-import { useRouter } from "vue-router";
+import { ref } from 'vue';
+import { UserOutlined, LockOutlined, TabletOutlined,MailOutlined  } from '@ant-design/icons-vue';
+
+const activeKey = ref('1');
 </script>
 <style lang="scss">
 .main {
-  margin-top: 100px;
+  margin-top: 200px;
   .top {
     text-align: center;
     .logo {
@@ -41,7 +73,6 @@ import { useRouter } from "vue-router";
   .card {
     width: 470px;
     border-radius: 5px;
-    border: 1px solid #c8c8c8;
     margin: 30px auto;
     padding: 50px;
     .title {
