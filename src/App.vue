c5f8bbd15208a3e490527c42a5a53595ac269b0e
gaoxiaoxinggxx <1129069725@qq.com>
2022-04-10 23:43:39 +0800

    feat: add siglnar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <template>
   <router-view />
 </template>
+<script lang="ts" setup>
+import {onMounted,onUnmounted} from 'vue'
+import { HubConnectionState } from '@microsoft/signalr';
+import hubConnection from '../src/util/signalRUitl';
+
+onMounted(async ()=>{
+  if(hubConnection.state != HubConnectionState.Connected){
+    await hubConnection.start();
+    console.info('started signalR...');
+  }
+  hubConnection.on('candidateDisconnected', async () => {
+      console.log('candidateDisconnected 触发了');
+  });
+
+  onUnmounted(()=>{
+    hubConnection?.off('candidateDisconnected');
+    console.info('off candidateDisconnected event.');
+  })
+
+})
+</script>
 
